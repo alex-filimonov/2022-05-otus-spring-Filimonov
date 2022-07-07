e7392401e2ac34dbb362bc7a732fd004d6d8cfdc
Alex Filimonov <filimonovao@rt.ru>
2022-07-08 09:27:46 +1000

    create spring 06

---
+++ spring-06/src/test/java/ru/otus/spring/spring06/Spring06ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.spring.spring06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
