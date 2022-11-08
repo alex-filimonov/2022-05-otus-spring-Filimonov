02fc6b999f6b2f1b70b7308962bb5f0748868087
Alex Filimonov <filimonovao@rt.ru>
2022-11-08 14:09:02 +1000

    spring 18

---
+++ spring-18/src/test/java/ru/otus/spring/spring18/Spring18ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.spring.spring18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring18ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
